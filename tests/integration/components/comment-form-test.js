38ff7db05be10520367ef937bf423b5017391b29
Terrance Bryant <terrancebryant85@gmail.com>
2016-09-06 14:10:50 -0600

    Testing Actions

---
+++ tests/integration/components/comment-form-test.js
@@ -0,0 +1,25 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('comment-form', 'Integration | Component | comment form', {
+  integration: true
+});
+
+test('should trigger external action on form submit', function(assert) {
+
+  // test double for the external action
+  this.set('externalAction', (actual) => {
+   let expected = { comment: 'You are not a wizard!' };
+   assert.deepEqual(actual, expected, 'submitted value is passed to external action');
+ });
+
+ this.render(hbs`{{comment-form submitComment=(action externalAction)}}`);
+
+  //  fill out the form and force an onchange
+  this.$('textarea').val('You are not a wizard!');
+  this.$('textarea').change();
+
+  //  click the button to submit the form
+  this.$('input').click();
+
+});
