54fbf07152759802cd6f036ed6cceea64efbfda8
Terrance Bryant <terrancebryant85@gmail.com>
2016-09-06 12:06:13 -0600

    Testing User Interaction

---
+++ app/components/magic-title.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+
+  title: 'Hello World',
+
+  actions: {
+    updateTitle() {
+      this.set('title', 'This is Magic');
+    }
+  }
+});
